582cd626ddf5984c585dc4b8dd132719c4ad8a33
deepanshuchhetri <167681539+deepanshuchhetri@users.noreply.github.com>
2024-04-23 13:41:34 +0545

    first commit

---
+++ firstapp/Program.cs
@@ -0,0 +1,28 @@
+﻿
+class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public Customer(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Customer customer1 = new Customer(101, "First Customer");
+        Customer customer2 = new Customer(102, "Second Customer");
+
+        // Access properties using proper casing
+        Console.WriteLine("ID of 1st Customer: " + customer1.Id);
+        Console.WriteLine("Name of 1st Customer: " + customer1.Name);
+
+        Console.WriteLine("ID of 2nd Customer:" + customer2.Id);
+       
+    }
+}
